d29517a4978d7c58686a58574a7b5fcfa50ab504
Vojtěch Hájek <vojtech.hajek2@gmail.com>
2023-04-21 09:24:42 +0200

    delete wrong makefiles

--- SharedPerunProfiler/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CXX_FLAGS="$CXX_FLAGS --no-undefined -Wno-invalid-noreturn -fPIC -fms-extensions -HOST_64BIT -BIT64 -UNICODE -PLATFORM_UNIX -PAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++17"
-INCLUDES="-I ../coreclr/pal/inc/rt -I ../coreclr/pal/src/include -I ../coreclr/pal/prebuilt/inc -I ../coreclr/inc"
-
-clang++ -shared -o PerunNetProfilerLin.so $CXX_FLAGS $INCLUDES CoreProfilerFactory.cpp CoreProfiler.cpp DLLMain.cpp Mutex.cpp Logger.cpp OS.cpp
